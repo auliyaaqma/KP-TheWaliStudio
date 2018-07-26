e19c9784464494b2621cd11e2ead7197798bbc38
nurindah yuniarti <31493745+NurindahYuniarti2A@users.noreply.github.com>
2018-07-26 10:50:26 +0700

    database sql

---
+++ db_sekolah.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.16-MariaDB : Database - db_sekolah
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_sekolah` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_sekolah`;
+
+/*Table structure for table `t_murid` */
+
+DROP TABLE IF EXISTS `t_murid`;
+
+CREATE TABLE `t_murid` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) DEFAULT NULL,
+  `kelas` varchar(50) DEFAULT NULL,
+  `jurusan` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
+
+/*Data for the table `t_murid` */
+
+insert  into `t_murid`(`id`,`nama`,`kelas`,`jurusan`) values (1,'Jhonniesss','XII RPL 1','Rekayasa Perangkat Lunak'),(2,'Muhammad Ilham Hidayat','XII RPL 2','Rekayasa Perangkat Lunak'),(3,'Budi Doremi ','XII TKJ','Teknik Komputer Jaringan'),(4,'John Dope','XII TKJ','Teknik Komputer Jaringan');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
